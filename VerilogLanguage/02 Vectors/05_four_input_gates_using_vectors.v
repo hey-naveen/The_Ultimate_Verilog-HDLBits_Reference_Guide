302742a49fd592b44a0b72dea54c9d3d066401ea
Naveen Kumar Gupta <124282752+nvikumar@users.noreply.github.com>
2026-01-02 14:08:37 +0530

    Update four input gates implementation
    Commented out previous implementations of logical gates.

--- VerilogLanguage/02 Vectors/05_four_input_gates_using_vectors.v
+++ VerilogLanguage/02 Vectors/05_four_input_gates_using_vectors.v
@@ -8,4 +8,12 @@ module top_module(
     assign out_or = |in;
     assign out_xor = ^in;
 
+    // m = 2
+
+    /*
+    assign out_and = (in[3] & in[2] & in[1] & in[0]);
+    assign out_or  = (in[3] | in[2] | in[1] | in[0]);
+    assign out_xor = (in[3] ^ in[2] ^ in[1] ^ in[0]);
+    */
+
 endmodule
