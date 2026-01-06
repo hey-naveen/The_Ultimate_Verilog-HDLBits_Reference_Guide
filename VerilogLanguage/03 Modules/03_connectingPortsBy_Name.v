af457cfb6cc8bee2b288c5eb3504e7d8112f2500
Naveen Kumar Gupta <124282752+nvikumar@users.noreply.github.com>
2026-01-06 14:11:57 +0530

    Add explicit port connection in module instantiation
    Added explicit port connection for module instantiation.

---
+++ VerilogLanguage/03 Modules/03_connectingPortsBy_Name.v
@@ -0,0 +1,20 @@
+module top_module ( 
+    input a, 
+    input b, 
+    input c,
+    input d,
+    output out1,
+    output out2
+);
+
+  // explicit port connection
+    mod_a inst(
+        .in1(a),
+        .in2(b),
+        .in3(c),
+        .in4(d),
+        .out1(out1),
+        .out2(out2)   
+    );
+
+endmodule
