9affec066ee734f99430e5b9b699d6b20867e3fe
Naveen G. <124282752+nvikumar@users.noreply.github.com>
2026-01-12 14:52:45 +0530

    Create 05_population_count_255bits_for_loop.v

---
+++ VerilogLanguage/05 More Verilog Features/05_population_count_255bits_for_loop.v
@@ -0,0 +1,25 @@
+
+
+module top_module( 
+    input [254:0] in,
+    output [7:0] out );
+    
+    always @(*)begin
+        out = 0;
+      for (integer i = 0; i < $bits(in); i++)
+            out = out + in[i];
+    end
+
+  /*
+    always @(*) begin
+     out = 0;
+     for(integer i = 0; i < $bits(in); i++) begin
+         if(in[i])
+             out = out + 8'b1;
+         else
+             out = out + 8'b0;
+     end
+  end
+  */
+
+endmodule
