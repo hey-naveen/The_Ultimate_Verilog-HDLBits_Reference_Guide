9e136d55247a3021d01f8473a5a9fdb38bda08dd
Naveen G. <124282752+nvikumar@users.noreply.github.com>
2026-01-13 11:15:57 +0530

    Update 07_BCDAdder_100bits_generateLoop (Add second method).v

--- VerilogLanguage/05 More Verilog Features/07_BCDAdder_100bits_generateLoop.v
+++ VerilogLanguage/05 More Verilog Features/07_BCDAdder_100bits_generateLoop.v
@@ -14,4 +14,28 @@ module top_module(
             end        
     endgenerate
     assign cout = carry[99];
+
+    // m = 2
+    /*
+    	bcd_fadd f0(
+		.a(a[3:0]),
+		.b(b[3:0]),
+		.cin(cin),
+		.cout(carry[0]),
+		.sum(sum[3:0])
+	);
+	
+  generate
+		genvar i;
+		for(i=1; i<100; i=i+1) begin : block1
+			bcd_fadd f(
+				.a(a[4*i+3 : 4*i]),
+				.b(b[4*i+3 : 4*i]),
+				.cin(carry[i-1]),
+				.cout(carry[i]),
+				.sum(sum[4*i+3 : 4*i])
+			);
+		end
+	endgenerate	
+    */
 endmodule
