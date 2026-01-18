83a5a357e41aa76c390fb24480f25dae9664a865
Naveen G. <124282752+nvikumar@users.noreply.github.com>
2026-01-18 19:15:59 +0530

    Add 9-to-1 multiplexer module in Verilog

---
+++ 03_Combinational_Circuits/02_Multiplexers/03_mux9to1.v
@@ -0,0 +1,22 @@
+module top_module( 
+    input [15:0] a, b, c, d, e, f, g, h, i,
+    input [3:0] sel,
+    output reg [15:0] out );
+    
+    always @(*) begin
+        out = '1; 	// Apostrophe Literal: used in system verilog. sets all bits to 1 regardless of the widths.
+        case (sel)
+            0 : out = a;
+            1 : out = b;
+            2 : out = c;
+            3 : out = d;
+            4 : out = e;
+            5 : out = f;
+            6 : out = g;
+            7 : out = h;
+            8 : out = i;
+            // no need of default statement
+        endcase
+    end
+
+endmodule
