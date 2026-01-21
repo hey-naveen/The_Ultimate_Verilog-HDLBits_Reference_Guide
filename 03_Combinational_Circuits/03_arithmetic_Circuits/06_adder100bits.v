2f9ee00be7e1e6bc446f81b0a8059ca938efd1c0
Naveen G. <124282752+nvikumar@users.noreply.github.com>
2026-01-21 11:37:51 +0530

    Enhance comments in 100-bit adder module
    Added comments to clarify the functionality of the adder.

---
+++ 03_Combinational_Circuits/03_arithmetic_Circuits/06_adder100bits.v
@@ -0,0 +1,11 @@
+module top_module( 
+    input [99:0] a, b,
+    input cin,
+    output cout,
+    output [99:0] sum );
+
+  // m = 1 : using direct addition (same as ripple carry addition)
+  // The concatenation {cout, sum} is a 101-bit vector.
+    assign {cout, sum } = a + b + cin;
+
+endmodule
